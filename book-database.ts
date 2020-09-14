77aa0cd55b2dcea638df7785fa7a1c576a1e8ee7
Erkka N <erkka.nieminen@me.com>
2020-09-14 18:13:56 +0300

    added more functions

--- book-database.ts
+++
@@ -1,3 +0,0 @@
-const fs = require('fs');
-const books = JSON.parse( fs.readFileSync('books.json', 'utf8') );
-console.log(books);
--- books/books.ts
+++ books/books.ts
@@ -1,9 +1,78 @@
 const fs = require('fs');
-const books = JSON.parse( fs.readFileSync('books.json', 'utf8') );
 
-function getBookByISBN(isbn: string) {
-	const book = books.find((book:any) => book.isbn === isbn);
-	return book;
+
+class Books {
+	private bookDBfile = 'books.json';
+	private library: Book[];
+
+	constructor() {
+		this.library = JSON.parse( fs.readFileSync(this.bookDBfile, 'utf8') );
+	}
+
+  add(book: Book) {
+  	this.library.push(new Book());
+  }
+
+	function saveDB() {
+		fs.writeFileSync(bookDBfile, JSON.stringify(books));
+	}
+	
+	function getBookByISBN(isbn: string) {
+		const book = books.find((book:any) => book.isbn === isbn);
+		return book;
+	}
+	//console.log(getBookByISBN('9781449325862'));
+	
+	function getBook(author: string, title: string) {
+		const book = books.find((book:any) => book.author === author && book.title === title);
+		return book;
+	}
+	
 }
 
-console.log(getBookByISBN('9781449325862'));
+interface Copy {
+	id: string,
+	status: 'borrowed' | 'in_library' | 'not_available',
+	due: Date,
+	borrower_id: string,
+}
+
+class Book {
+	private isbn: string;
+	private title: string;
+	private subtitle: string;
+	private author: string;
+	private published: Date;
+	private publisher: string;
+	private pages: number;
+	private description: string;
+	private website: URL;
+	private copies: Copy[];
+
+	constructor(isbn: string, title: string) {
+		this.isbn = name;
+		this.title = title;
+	}
+
+	private printDate(dateString: string) {
+		return new Date(dateString).toLocaleDateString();
+	}
+
+	private getAvailableCopies(copies: Copy[]): number {
+		let available = 0;
+		copies.forEach((copy:Copy) => {
+			if (copy.status === 'in_library')
+				++available;
+		});
+		return available;
+	}
+
+	public printBookDetails(book: Book) {
+		const year = new Date(book.published).getFullYear();
+		const titl = `${book.title} by ${book.author} (${year})`;
+		const totalBooks = `Books in library: ${book.copies.length}`;
+		const availableBooks = `Available for borrowing: ${this.getAvailableCopies(book.copies)}`;
+		return `${titl}\n${totalBooks}\n${availableBooks}`;
+	}
+	
+} // class Book

7d1e680be11379f186be998024f8a5ab1977b384
Erkka N <erkka.nieminen@me.com>
2020-09-15 22:35:51 +0300

    searching, returning and borrowing books

--- books/books.ts
+++ books/books.ts
@@ -1,41 +1,92 @@
 const fs = require('fs');
 
-
 class Books {
 	private bookDBfile = 'books.json';
-	private library: Book[];
+	private books: Book[];
 
 	constructor() {
-		this.library = JSON.parse( fs.readFileSync(this.bookDBfile, 'utf8') );
+		this.books = JSON.parse( fs.readFileSync(`${__dirname}/${this.bookDBfile}`, 'utf8') );
+	}
+
+	saveDB() {
+		fs.writeFileSync(this.bookDBfile, JSON.stringify(this.books));
 	}
 
   add(book: Book) {
-  	this.library.push(new Book());
+		this.books.push(new Book(book));
+		this.saveDB();
   }
 
-	function saveDB() {
-		fs.writeFileSync(bookDBfile, JSON.stringify(books));
-	}
-	
-	function getBookByISBN(isbn: string) {
-		const book = books.find((book:any) => book.isbn === isbn);
+	getBookByISBN(isbn: string): Book {
+		const book = this.books.find((book:any) => book.isbn === isbn);
 		return book;
 	}
 	//console.log(getBookByISBN('9781449325862'));
-	
-	function getBook(author: string, title: string) {
-		const book = books.find((book:any) => book.author === author && book.title === title);
+
+	getBook(author: string, title: string): Book {
+		const book = this.books.find((book:any) => book.author === author && book.title === title);
 		return book;
 	}
-	
-}
+
+	printBorrowedBooks(borrower_id: number) {
+		let borrowed = `Books you've borrowed:\n`;
+		this.books.forEach((book: Book)=> {
+			book.copies.forEach((copy: Copy) => {
+				if (parseInt(copy.borrower_id) === borrower_id) {
+					const due_date = new Date(copy.due_date).toLocaleDateString();
+					borrowed += `\n${book.getTitleAuthorYear()}\nDue ${due_date}`;
+				}
+			});
+		});
+	}
+
+	// returns books searched by its title or author name
+	findBook(searchString: string): Book[] {
+		const result = this.books.filter((book: Book)=> {
+			const haystack = book.title + book.author;
+			const idx = haystack.search(searchString);
+			if (idx !== -1) return true; // match found, push book to found results
+		});
+		return result;
+	}
+
+	// returns true if success, false if failure
+	borrowBook(book: Book, borrower_id: number): Boolean {
+		const availableCopy: Copy = book.copies.find((copy: Copy) => copy.status === 'in_library');
+		if (availableCopy === undefined) return false; // no books are available
+		else {
+			availableCopy.borrower_id = borrower_id.toString();
+			availableCopy.status = 'borrowed';
+			const date = new Date();
+			date.setDate(date.getDate() + 28); // four weeks from now :D
+			availableCopy.due_date = date;
+			// TODO: add to user's books
+		}
+		return true;
+	}
+
+	// returns true if success, false if failure
+	returnBook(book: Book, borrower_id: number): Boolean {
+		const borrowedCopy: Copy = book.copies.find((copy: Copy) => copy.status === 'borrowed' && parseInt(copy.borrower_id) === borrower_id);
+		if (borrowedCopy === undefined) return false; // the book is not currently borrowed by the borrower
+		else {
+			borrowedCopy.borrower_id = null;
+			borrowedCopy.status = 'in_library';
+			borrowedCopy.due_date = null;
+			// TODO: remove from user's books and add to user's history
+		}
+		return true;
+	}
+} // class Books
+
 
 interface Copy {
 	id: string,
 	status: 'borrowed' | 'in_library' | 'not_available',
-	due: Date,
+	due_date: Date,
 	borrower_id: string,
 }
+
 
 class Book {
 	private isbn: string;
@@ -47,32 +98,43 @@ class Book {
 	private pages: number;
 	private description: string;
 	private website: URL;
-	private copies: Copy[];
+	public copies: Copy[];
 
-	constructor(isbn: string, title: string) {
-		this.isbn = name;
-		this.title = title;
+	constructor(book: Book) {
+		this.isbn = book.isbn;
+		this.title = book.title;
+		this.subtitle = book.subtitle;
+		this.author = book.author;
+		this.published = book.published;
+		this.publisher = book.publisher;
+		this.pages = book.pages;
+		this.description = book.description;
+		this.website = book.website;
+		this.copies = book.copies;
 	}
 
-	private printDate(dateString: string) {
+	private printDate(dateString: string): string {
 		return new Date(dateString).toLocaleDateString();
 	}
 
-	private getAvailableCopies(copies: Copy[]): number {
+	private getAvailableCopies(): number {
 		let available = 0;
-		copies.forEach((copy:Copy) => {
+		this.copies.forEach((copy:Copy) => {
 			if (copy.status === 'in_library')
 				++available;
 		});
 		return available;
 	}
 
-	public printBookDetails(book: Book) {
-		const year = new Date(book.published).getFullYear();
-		const titl = `${book.title} by ${book.author} (${year})`;
-		const totalBooks = `Books in library: ${book.copies.length}`;
-		const availableBooks = `Available for borrowing: ${this.getAvailableCopies(book.copies)}`;
-		return `${titl}\n${totalBooks}\n${availableBooks}`;
+	getTitleAuthorYear(): string {
+		const year = new Date(this.published).getFullYear();
+		return `${this.title} by ${this.author} (${year})`;
 	}
-	
+
+	printDetails(): string {
+		const title = this.getTitleAuthorYear();
+		const totalBooks = `Books in library: ${this.copies.length}`;
+		const availableBooks = `Available for borrowing: ${this.getAvailableCopies()}`;
+		return `${title}\n${totalBooks}\n${availableBooks}`;
+	}
 } // class Book
